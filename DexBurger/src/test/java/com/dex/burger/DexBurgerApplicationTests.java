60f54c887244e9a104b83cdd77eeb4bc362d8a0b
Gustavo Frederico Temple Pedrosa <gustavo.temple0@gmail.com>
2019-04-24 16:28:34 -0300

    Spring Initializr

---
+++ DexBurger/src/test/java/com/dex/burger/DexBurgerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dex.burger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DexBurgerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
